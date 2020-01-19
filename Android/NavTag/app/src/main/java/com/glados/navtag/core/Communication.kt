6f424f15938948c6c00456f627202d3ab1667e04
Randy Sab-Roy <randy.sab.roy@gmail.com>
2020-01-19 08:30:43 -0500

    It fucking works boys

--- Android/NavTag/app/src/main/java/com/glados/navtag/core/Communication.kt
+++ Android/NavTag/app/src/main/java/com/glados/navtag/core/Communication.kt
@@ -1,6 +1,9 @@
 package com.glados.navtag.core
 
+import android.util.Log
 import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.android.extension.responseJson
+import com.github.kittinunf.fuel.httpGet
 
 object Communication {
     private const val IP = "http://192.168.137.27:8080/"
@@ -14,10 +17,10 @@ object Communication {
     }
 
     private fun setOff() {
-        Fuel.get(IP + "000000000000000000000000000000")
+        Fuel.get(IP + "000000000000000000000000000000").responseString{ _, response, result ->}
     }
 
     fun setSafetyLight(){
-        Fuel.get(IP + "171711717117171020200202002020")
+        Fuel.get(IP + "171711717117171020200202002020").responseString{ _, response, result ->}
     }
 }
