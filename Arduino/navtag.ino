442e6e393f1fc8a97c20b3f552c09c19d411eb41
Randy Sab-Roy <randy.sab.roy@gmail.com>
2020-01-19 01:19:44 -0500

    To arduino nano

--- Arduino/navtag.ino
+++ Arduino/navtag.ino
@@ -1,24 +1,10 @@
 #include <Adafruit_NeoPixel.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
 
-#define NUM_LEDS 6
-#define DATA_PIN 14
-#define GROUND_PIN 15
-#define GROUND2_PIN 12
-#define LOWV_PIN 13
+#define NUM_LEDS 15
+#define DATA_PIN 13
 
-#define WIFI_SSID "NavTag"
-#define WIFI_PASSWORD "12345678"
-#define HOSTNAME "WeMos"
 
 Adafruit_NeoPixel pixels(NUM_LEDS, DATA_PIN, NEO_GRB | NEO_KHZ800);
-WiFiServer server(80);
-WiFiClient client;
-unsigned long lastUpdate = 0;
-
-uint32_t colors[NUM_LEDS];
-uint32_t freqs[NUM_LEDS];
 
 const uint32_t BLACK = pixels.Color(0, 0, 0);
 const uint32_t RED = pixels.Color(255, 0, 0);
@@ -39,15 +25,9 @@ void lightAllLeds(uint32_t color)
 void setupPin()
 {
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(GROUND_PIN, OUTPUT);
-    pinMode(GROUND2_PIN, OUTPUT);
-    pinMode(LOWV_PIN, OUTPUT);
     pinMode(DATA_PIN, OUTPUT);
 
     digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(GROUND_PIN, LOW);
-    digitalWrite(GROUND2_PIN, LOW);
-    digitalWrite(LOWV_PIN, HIGH);
 }
 
 void setupLed()
@@ -55,90 +35,6 @@ void setupLed()
     pixels.setBrightness(100);
     pixels.begin();
     pixels.clear();
-    pixels.show();
-}
-
-void setupWifi()
-{
-    Serial.println("");
-    Serial.print("Connecting to ");
-    Serial.print(WIFI_SSID);
-    WiFi.mode(WIFI_STA);
-    WiFi.hostname(HOSTNAME);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("");
-    Serial.print("WiFi connected with address ");
-    Serial.println(WiFi.localIP());
-}
-
-void serveClient(WiFiClient &client)
-{
-	// Wait for client
-	for (int i = 0; i < 1000 && !client.available(); i++)
-	{
-		delay(1);
-	}
-	if (!client.available())
-	{
-		return;
-	}
-
-	// Read client request
-	String req = client.readStringUntil('e');
-    int length = req.length();
-
-    // Set colors
-    for(int i = 0; (i < length) && (i < NUM_LEDS); i++)
-    {
-        switch(req[i])
-        {
-            case '0':
-                colors[i] = BLACK;
-                break;
-            case '1':
-                colors[i] = RED;
-                break;
-            case '2':
-                colors[i] = GREEN;
-                break;
-            case '3':
-                colors[i] = BLUE;
-                break;
-            case '4':
-                colors[i] = WHITE;
-                break;
-            default:
-                colors[i] = BLACK;
-                break;
-        }
-    }
-
-    // Set freqs
-    for(int i = 0; (i+NUM_LEDS < length) && (i < NUM_LEDS); i++)
-    {
-        int freq = req[i+NUM_LEDS] - '0';
-        if(freq >= 0 && freq <= 9)
-        {
-            freqs[i] = freq;
-        }
-    }
-
-    client.flush();
-    client.print("OK\n");
-}
-
-void updateColors()
-{
-    for(int i = 0; i < NUM_LEDS; i++)
-    {
-        pixels.setPixelColor(i, (freqs[i] > 0 && (millis() % (freqs[i] * 200) > ((freqs[i] * 200) / 2))) ? BLACK : colors[i]);
-    }
-    Serial.print("\n");
     pixels.show();
 }
 
@@ -151,12 +47,6 @@ void setup()
     setupPin();
     setupLed();
 
-    // WiFi connection
-    setupWifi();
-
-    // Setup server
-    server.begin();
-
     lightAllLeds(GREEN);
     delay(500);
     lightAllLeds(BLACK);
@@ -164,12 +54,11 @@ void setup()
 
 void loop()
 {
-    client = server.available();
-    if (client && client.connected())
-    {
-        serveClient(client);
-        client.stop();
-    }
-
-    updateColors();
+    pixels.setPixelColor(3, BLUE);
+    pixels.setPixelColor(5, BLUE);
+    pixels.setPixelColor(6, BLUE);
+    pixels.setPixelColor(7, BLUE);
+    pixels.setPixelColor(8, BLUE);
+    pixels.setPixelColor(13, BLUE);
+    pixels.show();
 }
