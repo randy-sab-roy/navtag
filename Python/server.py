e7232a1b16e90723b7843d9c9d6fa3ede717d729
Randy Sab-Roy <randy.sab.roy@gmail.com>
2020-01-19 06:53:48 -0500

    Repair python

--- Python/server.py
+++ Python/server.py
@@ -6,14 +6,18 @@ from BaseHTTPServer import BaseHTTPRequestHandler
 
 ser = serial.Serial('/dev/ttyUSB0')
 ser.baudrate = 115200
-def some_function():
-    print "some_function got called"
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-	ser.write(self.path[1:])
+        ser.write(self.path[1:])
         self.send_response(200)
-	time.sleep(1)
+        time.sleep(1)
+
 
 httpd = SocketServer.TCPServer(("", 8080), MyHandler)
-httpd.serve_forever()
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+httpd.server_close()
+ser.close()
