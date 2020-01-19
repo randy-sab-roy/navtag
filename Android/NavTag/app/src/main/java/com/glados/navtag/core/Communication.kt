1482ac5319b4c57b8a65d29e03947e5b6417091e
Randy Sab-Roy <randy.sab.roy@gmail.com>
2020-01-19 09:08:09 -0500

    All modes

--- Android/NavTag/app/src/main/java/com/glados/navtag/core/Communication.kt
+++ Android/NavTag/app/src/main/java/com/glados/navtag/core/Communication.kt
@@ -13,14 +13,29 @@ object Communication {
         {
             NavTagMode.Off -> setOff()
             NavTagMode.SafetyLight -> setSafetyLight()
+            NavTagMode.Destination -> setDestination()
+            NavTagMode.Commute -> setCommute()
+            NavTagMode.Running -> setRunning()
         }
     }
 
     private fun setOff() {
-        Fuel.get(IP + "000000000000000000000000000000").responseString{ _, response, result ->}
+        Fuel.get(IP + "000000000000000000000000000000").responseString{ _, _, _ ->}
     }
 
-    fun setSafetyLight(){
-        Fuel.get(IP + "171711717117171020200202002020").responseString{ _, response, result ->}
+    private fun setSafetyLight(){
+        Fuel.get(IP + "171711717117171020200202002020").responseString{ _, _, _ ->}
+    }
+
+    private fun setDestination(){
+        Fuel.get(IP + "000404411000040000000000000000").responseString{ _, _, _ ->}
+    }
+
+    private fun setCommute(){
+        Fuel.get(IP + "055500555005550444444444444444").responseString{ _, _, _ ->}
+    }
+
+    private fun setRunning(){
+        Fuel.get(IP + "111110000033330000000000000020").responseString{ _, _, _ ->}
     }
 }
